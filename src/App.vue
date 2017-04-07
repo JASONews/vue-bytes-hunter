741b134824b700de504ec598ea4392382ac9d3e9
Yida <yidayuan1028@gmail.com>
2017-04-07 17:02:29 -0400

    change user options

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,15 @@
                 <button type="submit" class="btn btn-default">Search</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a data-toggle="modal" data-target="#signupModal">Sign Up</a></li>
-                <li><a data-toggle="modal" data-target="#loginModal">login</a></li>
-                <router-link tag="li" to="/user-page"><a><span class="glyphicon glyphicon-user"></span></a></router-link>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <router-link tag="li" to="/user-page"><a>Profile</a></router-link>
+                        <router-link tag="li" to="/user-page"><a>Help</a></router-link>
+                        <li><a data-toggle="modal" data-target="#signupModal">Setting</a></li>
+                        <li class="divider"></li>
+                        <li><a data-toggle="modal" data-target="#signoutModal">Sign Out</a></li>
+                    </ul>
+                </li>
             </ul>
         </div>
       </div>
@@ -72,6 +78,27 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Login</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div id="signoutModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Sign Out</h4>
+          </div>
+          <div class="modal-body">
+            <p>Are you sure to sign out Bytes Hunter?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Sign Out</button>
             <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
           </div>
         </div>
