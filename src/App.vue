1c8d274e9d0e8f8090a5a514edf6e25dc2c5da62
Yida <yidayuan1028@gmail.com>
2017-04-06 20:00:02 -0400

    add signup/login modal

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,8 @@
                 <button type="submit" class="btn btn-default">Search</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <router-link tag="li" to="/signup"><a>Sign Up</a></router-link>
-                <router-link tag="li" to="/login"><a>Login</a></router-link>
+                <li><a data-toggle="modal" data-target="#signupModal">Sign Up</a></li>
+                <li><a data-toggle="modal" data-target="#loginModal">login</a></li>
                 <router-link tag="li" to="/user-page"><a><span class="glyphicon glyphicon-user"></span></a></router-link>
             </ul>
         </div>
@@ -35,11 +35,53 @@
         <router-view></router-view>
       </div>
     </div>
+
+    <!-- Modal -->
+    <div id="signupModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Sign Up</h4>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the modal.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div id="loginModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Login</h4>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the modal.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 </script>
+
 
 <style>
 body { 
