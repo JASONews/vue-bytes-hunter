3d5886b8b76c62d78b0909c4ceecc37cf3eb46ee
Yida <yidayuan1028@gmail.com>
2017-04-07 21:49:41 -0400

    user profile update

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -26,102 +26,40 @@
           </div>
 
           <div class="col-sm-10">
-              <div class="container">
-                <div class="page-header">
-                    <h1 id="timeline">Your Journey as A Bytes Hunter</h1>
-                </div>
-                <ul class="timeline">
-                    <li>
-                      <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">CatByte</h4>
-                          <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago</small></p>
-                        </div>
-                        <div class="timeline-body">
-                          <p>You successfully completed <i><strong>Challenge: CatByte</strong></i></p>
-                        </div>
-                      </div>
-                    </li>
-                    <li class="timeline-inverted">
-                      <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>You successfully completed <i><strong>Challenge: CatByte</strong></i></p>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                        </div>
-                      </div>
-                    </li>
-                    <li class="timeline-inverted">
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div class="timeline-badge info"><i class="glyphicon glyphicon-floppy-disk"></i></div>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                          <hr>
-                          <div class="btn-group">
-                            <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
-                              <i class="glyphicon glyphicon-cog"></i> <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="#">Action</a></li>
-                              <li><a href="#">Another action</a></li>
-                              <li><a href="#">Something else here</a></li>
-                              <li class="divider"></li>
-                              <li><a href="#">Separated link</a></li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                        </div>
-                      </div>
-                    </li>
-                    <li class="timeline-inverted">
-                      <div class="timeline-badge success"><i class="glyphicon glyphicon-thumbs-up"></i></div>
-                      <div class="timeline-panel">
-                        <div class="timeline-heading">
-                          <h4 class="timeline-title">Mussum ipsum cacilds</h4>
-                        </div>
-                        <div class="timeline-body">
-                          <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                        </div>
-                      </div>
-                    </li>
-                </ul>
+            <div class="container">
+              <div class="page-header">
+                  <h1 id="timeline-text">Your Journey as A Bytes Hunter</h1>
               </div>
+              <div class="cards-container">
+                <div class="card mb-3">
+                  <img class="card-img-top" src="../assets/dragon-byte.jpg" alt="Card image cap">
+                  <div class="card-block">
+                    <h4 class="card-title">DragonByte</h4>
+                    <p>You've successfully completed <i><strong>Challenge: DragonByte</strong></i></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+
+                <div class="card mb-3">
+                  <img class="card-img-top" src="../assets/dragon-byte.jpg" alt="Card image cap">
+                  <div class="card-block">
+                    <h4 class="card-title">PhoneixByte</h4>
+                    <p>You've successfully completed <i><strong>Challenge: PhoneixByte</strong></i></p>
+                    <p class="card-text"><small class="text-muted">Last updated 1 hour ago</small></p>
+                  </div>
+                </div>
+
+                <div class="card mb-3">
+                  <img class="card-img-top" src="../assets/dragon-byte.jpg" alt="Card image cap">
+                  <div class="card-block">
+                    <h4 class="card-title">ElephantByte</h4>
+                    <p>You've successfully completed <i><strong>Challenge: ElephantByte</strong></i></p>
+                    <p class="card-text"><small class="text-muted">Last updated 2 days ago</small></p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
       </div>
   </div>
